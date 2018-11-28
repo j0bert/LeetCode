44a92a57658d796b64b92d2e65ff8c5d34a70c30
Joel Snider <38743987+j0bert@users.noreply.github.com>
2018-11-28 13:48:45 -0500

    Update PerfectNumbers.java

--- PerfectNumbers.java
+++ PerfectNumbers.java
@@ -1,3 +1,7 @@
+/*We define the Perfect Number is a positive integer that is equal to the sum of all its positive divisors except itself.
+
+Now, given an integer n, write a function that returns true when it is a perfect number and false when it is not.*/
+
 public class PerfectNumbers {
 
 	public static void main(String[] args) {
